f2b2ca90741d9571e019ed6ef557573bf89d9e7d
ryo.watanabe <susiyaki.dev@gmail.com>
2019-08-18 05:44:07 +0900

    現状formStylesを使っていなかったので削除

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -76,7 +76,6 @@ export default class extends Component {
           handleChange={text => this.handleChange("fuel", text)}
           placeholder="ex）15km/l→15"
           errorMessage={errorMessage.fuel}
-          formStyles={formStyles}
         />
         <Form
           label="ガソリン相場"
@@ -84,41 +83,9 @@ export default class extends Component {
           handleChange={text => this.handleChange("cost", text)}
           placeholder="ex）140円/l→140"
           errorMessage={errorMessage.cost}
-          formStyles={formStyles}
         />
         <Button text="保存" onPress={this.save} />
       </View>
     );
   }
 }
-
-const formStyles = StyleSheet.create({
-  labelContainer: {
-    paddingTop: 40,
-    width: "90%",
-    alignItems: "flex-start"
-  },
-  label: {
-    fontWeight: "bold",
-    color: colors.black,
-    fontSize: 24,
-    marginBottom: 8
-  },
-  input: {
-    height: 56,
-    width: "90%",
-    paddingLeft: 8,
-    fontSize: 24,
-    backgroundColor: colors.white,
-    color: colors.black,
-    fontWeight: "bold",
-    borderRadius: 8
-  },
-  error: {
-    marginTop: 20,
-    marginBottom: 20,
-    fontSize: 16,
-    fontWeight: "bold",
-    color: colors.accent
-  }
-});
