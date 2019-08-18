c2448a62daf03d4d2c99a0d281cb51e98465dcbd
ryo.watanabe <susiyaki.dev@gmail.com>
2019-08-18 18:28:31 +0900

    共有ボタン実装

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Share, Button, Alert } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 import globalStyles from "../../assets/styleSheets/globalStyles";
 
 export default class extends Component {
+  onShare = async () => {
+    const {
+      region,
+      start_latLng,
+      end_latLng,
+      responseOrigin,
+      responseDestination,
+      distance,
+      duration,
+      useFuelAmount,
+      feeOfFuel,
+      payPerPerson
+    } = this.props.detailData;
+    try {
+      const result = await Share.share({
+        message: `
+        一人あたり${payPerPerson}円!
+        `
+      });
+      if (result.action === Share.sharedAction) {
+        console.log("successfully share!");
+      } else if (result.action === Share.dismissedAction) {
+        Alert.alert("共有に失敗しました");
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   render() {
     const {
       region,
@@ -33,6 +62,7 @@ export default class extends Component {
         </Text>
         <Text>ガソリン代:{Math.round(feeOfFuel)}円</Text>
         <Text>一人あたりの支払い:{Math.round(payPerPerson)}円</Text>
+        <Button title="share" onPress={this.onShare} />
       </View>
     );
   }
