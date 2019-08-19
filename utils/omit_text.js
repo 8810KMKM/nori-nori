59cd7346481805978e3f933c5bd2bd3dedb7a49f
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-08-19 14:13:52 +0900

    fix

--- utils/omit_text.js
+++ utils/omit_text.js
@@ -1,5 +1,9 @@
 export default locationName => {
-  const _locationName = locationName.split(/[、, 　]/);
+  // 句読点, カンマ, 半角スペース, 全角スペース
+  const SPLIT_CHAR = /[、, 　]/; 
+  const MAX_LENGTH = 15;
+
+  const _locationName = locationName.split(SPLIT_CHAR);
 
   const topLessName = _locationName.length > 1 
     ? _locationName.splice(1)
@@ -7,7 +11,7 @@ export default locationName => {
   
   const reversedName = topLessName.reverse().join(' ');
 
-  return reversedName.length > 15 
-    ? `${reversedName.slice(0, 15)}...`
+  return reversedName.length > MAX_LENGTH
+    ? `${reversedName.slice(0, MAX_LENGTH)}...`
     : reversedName
 }
